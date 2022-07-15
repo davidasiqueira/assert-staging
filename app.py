f24e6e44c92b96c77f67107a028a0695490a4f90
David <davidsiqueira.hog@gmail.com>
2022-07-15 13:58:25 -0300

    teste de ambiente

---
+++ app.py
@@ -0,0 +1,16 @@
+#from crypt import methods
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    
+    return render_template("index.html")
+
+@app.route("/great", methods=["POST"])
+def greet():
+
+    name = request.form.get("name")
+
+    return render_template("great.html", name=name)
