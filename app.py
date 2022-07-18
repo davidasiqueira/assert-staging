8c96431f34b1a35170670bc44478abf02f91fc03
David <davidsiqueira.hog@gmail.com>
2022-07-17 21:10:05 -0300

    api de teste

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 #from crypt import methods
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///assert.db3'
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
@@ -16,3 +21,11 @@ def greet():
     return render_template("great.html", name=name)
 
 
+@app.route("/api/formulario_candidato", methods=["POST"])
+def formulario_candidato():
+
+    body = request.get_json()
+    print(body)
+    return "200"
+
+    
