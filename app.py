c2f3b86f61a45d55f6372887b366a3806e1a579b
David <davidsiqueira.hog@gmail.com>
2022-08-10 08:16:06 -0300

    instalação do pacote jsonschema, criação do schema de validação dos dados recebidos pela api na pasta api_validation

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-#from crypt import methods
+
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+
 
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///assert.db3'
-db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
