f5d7717cd3181649b39da89c89dbe3ecc7ba3735
David Alvarez Siqueira <davidsiqueira.hog@gmail.com>
2022-08-17 21:34:13 -0300

    formulário de cadastro de empresa e inicio do formulário de cadastro de vaga

--- api_validation/validate.py
+++ api_validation/validate.py
@@ -3,40 +3,66 @@ from json import load
 
 
 def user(json):
-    
+
     try:
-        with open ("api_validation/user_schema.json") as f:
+        with open("api_validation/user_schema.json") as f:
             schema = load(f)
-            print(validate(json["user_data"], schema))
+            validate(json["user_data"], schema)
     except:
         return "Erro ao validar o schema{}".format(str(schema))
 
-    try: 
-        with open ("api_validation/soft_schema.json") as f:
+    try:
+        with open("api_validation/soft_schema.json") as f:
             schema = load(f)
             validate(json["user_soft"], schema)
     except:
         return "Erro ao validar o schema{}".format(str(schema))
 
-    try: 
-        with open ("api_validation/languages_schema.json") as f:
+    try:
+        with open("api_validation/languages_schema.json") as f:
             schema = load(f)
             validate(json["user_hard"]["languages"], schema)
     except:
         return "Erro ao validar o schema{}".format(str(schema))
 
-    try: 
-        with open ("api_validation/job_schema.json") as f:
+    try:
+        with open("api_validation/job_schema.json") as f:
             schema = load(f)
             validate(json["user_hard"]["jobs"], schema)
     except:
         return "Erro ao validar o schema{}".format(str(schema))
 
-    try: 
-        with open ("api_validation/courses_schema.json") as f:
+    try:
+        with open("api_validation/courses_schema.json") as f:
             schema = load(f)
             validate(json["user_hard"]["courses"], schema)
     except:
         return "Erro ao validar o schema{}".format(str(schema))
 
-    return True    
+    return True
+
+
+def company(json):
+
+    try:
+        with open("api_validation/company_schema.json") as f:
+            schema = load(f)
+            validate(json, schema)
+    except:
+        return "Erro ao validar o schema{}".format(str(schema))
+
+    return True
+
+
+def vaga(json):
+    
+    try:
+        with open("api_validation/vacancy_schema.json") as f:
+            schema = load(f)
+            validate(json, schema)
+    except:
+        return "Erro ao validar o schema{}".format(str(schema))
+
+    return True
+
+    
--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 
 from flask import Flask, render_template, request
 from api_validation import validate
-from db import manipulate_data
+import db
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    
+
     return render_template("index.html")
+
 
 @app.route("/great", methods=["POST"])
 def greet():
@@ -21,18 +23,55 @@ def greet():
 @app.route("/api/formulario_candidato", methods=["POST"])
 def formulario_candidato():
     erro = ''
-    #api de teste
+    # api de teste
     body = request.get_json()
     isvalid = validate.user(body)
 
-    if isvalid:
-        
+    if isvalid == True:
+
         try:
-            erro = manipulate_data(body)
+            erro = cadastra_usuario(body)
             return "Salvo com sucesso"
         except:
-            return erro       
-    
-    return isvalid
+            return erro
+    else:
 
-    
+        return isvalid
+
+
+app.route("/api/formulario_empresa", methods=["POST"])
+def formulario_empresa():
+    erro = ''
+    # api de teste
+    body = request.get_json()
+    isvalid = validate.company(body)
+
+    if isvalid == True:
+
+        try:
+            erro = db.cadastra_empresa(body)
+            return "Salvo com sucesso"
+        except:
+            return erro
+    else:
+
+        return isvalid
+
+
+app.route("/api/formulario_vaga", methods=["POST"])
+def formulario_vaga():
+    erro = ''
+    # api de teste
+    body = request.get_json()
+    isvalid = validate.vaga(body)
+
+    if isvalid == True:
+
+        try:
+            erro = db.cadastra_vaga(body)
+            return "Salvo com sucesso"
+        except:
+            return erro
+    else:
+
+        return isvalid
